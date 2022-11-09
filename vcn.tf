506580dd3d0d04f1cbcc44be15ae90c986dbcc75
OU-DEVELOPERS <115209504+ou-developers@users.noreply.github.com>
2022-11-09 20:43:06 +0530

    Update vcn.tf

--- vcn.tf
+++ vcn.tf
@@ -13,10 +13,12 @@ terraform {
     #display_name = "IAD-DOP-LAB03-1-VCN-01"
     #cidr_blocks = ["10.0.0.0/16"]
 #}
+
 /*
 resource "oci_core_subnet" "example_subnet" {
     compartment_id = "<your_assigned_compartment_ocid>"
     display_name = "IAD-DOP-LAB03-1-SNT-01"
     vcn_id = oci_core_vcn.example_vcn.id
     cidr_block = "10.0.0.0/24"
-}*/
+}
+*/
